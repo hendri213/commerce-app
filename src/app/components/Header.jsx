fbc41f091dbc09b89a8b4dcf160296ea64da0eff
Hendri Setiawan <hendri450450450@gmail.com>
2023-09-11 07:37:30 +0700

    menambahkan navbar dan tahapan awal carousel

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,46 +3,26 @@
 import React from 'react'
 import { BsCart, BsSearch } from 'react-icons/bs';
 
+
 const Header = () => {
   return (
-    <div className="fixed inset-x-0 top-0 w-full">
-      <div className="box-between md:px-10 px-7 py-4">
-
-          {/* name brand */}
-        <div className="box-equal">
-          <a className="font-bold text-5xl text-blue-600" href="#">Shoppedia</a>
+    <div className='fixed left-0 top-0 w-full mt-[-5px]'>
+      <div className="flex items-center justify-between md:px-10 px-7 py-4">
+        <div>
+          <a className="text-3xl font-bold text-blue-600" href="#">Shoppedia</a>
         </div>
-
-        {/* search feature */}
-        <div className="box-equal relative">
-          <div className='absolute inset-y-0 pl-3 box-equal pointer-events-none text-gray-500 dark:text-gray-400'>
-            <BsSearch className="w-5 h-5" />
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <BsSearch className="w-4 h-4 text-gray-500 dark:text-gray-400" />
           </div>
-          <input className='md:w-[1000px] rounded-lg p-2 pl-10 text-sm text-gray-900 border border-gray-300' type="text" placeholder='cari di shoppedia' />
+          <input className='block w-full p-4 pl-10 focus:outline-none  focus:border-blue-600 focus:ring-1 focus:ring-blue-600 md:w-[1200px] py-1 rounded-lg text-gray-900 border border-gray-300' type="text" placeholder="cari di shoppedia" />
         </div>
-
-        {/* link navigation */}
-        <div className="box-equal">
-          <ul className='box-equal'>
-            <li className="md:ml-4">
-              <a href="#">
-                <BsCart className="w-[25px] h-[25px]" />
-              </a>
-            </li>
-            <li className="md:ml-8">
-              <a className="font-bold text-blue-600 border border-blue-600 px-8 py-2 rounded-lg text-sm" href="#">
-                Masuk
-              </a>
-            </li>
-            <li className="md:ml-2">
-              <a  className="font-bold text-white border bg-blue-600 px-8 py-2 rounded-lg text-sm" href="#">
-                Daftar
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul className="flex items-center">
+          <li className="md:ml-2"><a href="#"><BsCart className="w-5 h-5" /></a></li>
+          <li className="md:ml-6"><a href="#" className="font-bold text-blue-600 text-sm border border-blue-600 px-4 py-2 rounded-lg">Masuk</a></li>
+          <li className="md:ml-2"><a href="#" className="font-bold text-white text-sm bg-blue-600 px-4 py-2 rounded-lg">Daftar</a></li>
+        </ul>
       </div>
-
     </div>
   )
 }
